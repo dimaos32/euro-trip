457f6eb281f7a99c86d0e4cb186363fe2ad59393
Dima Os <dimaos@protonmail.com>
2020-12-02 12:26:52 +0300

    Делаег главное меню активным

--- source/js/script.js
+++ source/js/script.js
@@ -4,17 +4,38 @@ var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
 var mainNavList = mainNav.querySelector('.main-nav__list');
 
+var openMenu = function () {
+  mainNav.classList.remove('main-nav--closed');
+  mainNav.classList.add('main-nav--opened');
+  mainNavList.addEventListener('click', onOutsideMenuPress);
+  document.addEventListener('keydown', onOpenMenuEscPress);
+};
+
+var closeMenu = function () {
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+  mainNavList.removeEventListener('click', onOutsideMenuPress);
+  document.removeEventListener('keydown', onOpenMenuEscPress);
+};
+
+var onOutsideMenuPress = function () {
+  closeMenu();
+};
+
+var onOpenMenuEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeMenu();
+  }
+};
+
 mainNav.classList.remove('main-nav--nojs');
 mainNav.classList.add('main-nav--closed');
 
 mainNavToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('main-nav--closed');
-  mainNav.classList.toggle('main-nav--opened');
-});
-
-mainNavList.addEventListener('click', function () {
   if (mainNav.classList.contains('main-nav--opened')) {
-    mainNav.classList.remove('main-nav--opened');
-    mainNav.classList.add('main-nav--closed');
+    closeMenu();
+  } else {
+    openMenu();
   }
 });
